17e72746834d21a2a90cff64a5af7a86ba422369
HenrikHaalandDev <henrikbibow@icloud.com>
2025-05-07 10:24:05 +0200

    placed html script into script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,14 @@
 function sendMelding() {
     alert("Melding sendt!");
 }
+
+function darkmode() {
+    /* funker ikke :() */
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+document.getElementById('comment-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const input = document.getElementById('comment').value;
+    document.getElementById('comment-section').innerHTML += `<p>${input}</p>`;
+});
